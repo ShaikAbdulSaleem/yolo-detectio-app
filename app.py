cd929c3e6a368cabd2e1b1bde52261c6fb036fea
ShaikAbdulSaleem <163165090+ShaikAbdulSaleem@users.noreply.github.com>
2025-05-23 12:43:44 -0400

    Update app.py

--- app.py
+++ app.py
@@ -38,4 +38,6 @@ def detect():
     return send_file(output_path, mimetype='image/jpeg')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
